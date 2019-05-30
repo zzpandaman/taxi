29cee117291d35f146182ccc6568dae0bc03db8c
zzpandaman <079502176461a>
2019-05-30 08:36:11 +0800

    0 failures

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+	let attributes = Object.keys(inputs);
+	let part1 = 0.25 * inputs[attributes[1]];
+	let part2 = 0;
+	let distance = inputs[attributes[0]];
+	if(distance < 2){
+		part2 = 6;
+	}else if(distance > 8){
+		part2 = 10.8 + 1.2 * (distance - 8);
+	}else{
+		part2 = 6 + 0.8 * (distance - 2);
+	}
+    return Math.round(part1 + part2);
 };
